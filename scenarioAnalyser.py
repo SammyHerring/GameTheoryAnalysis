3dba6beb021ca47acd140e67c588ea966e5bd3b6
SammyHerring <sammy.herring@me.com>
2017-12-30 12:57:08 +0000

    v1.1
    Code optimisation before moving on to next step. Further development
    may occur, time dependant.

--- scenarioAnalyser.py
+++ scenarioAnalyser.py
@@ -6,11 +6,11 @@ import numpy as np
 #Proof of Concept to find the dominant solutions to produce a nach equilibrium where difference values are equal.
 #We may prove the nach equilibrium result by selecting the intersection with the maximum original input value.
 
-#Multiple Nach Equilibrium Test
-##player1 = [[3, 8],
-##           [0, 4]]
-##player2 = [[3, 0],
-##           [8, 4]]
+#Multiple Nach Equilibrium Test Data
+player12x2 = [[3, 8],
+           [0, 4]]
+player22x2 = [[3, 0],
+           [8, 4]]
 
 #Matrices re-written in column-down format
 player1 = [[7, 2, 3, 5],
@@ -23,10 +23,19 @@ player2 = [[1, 1, 7, 4],
            [4, 4, 90, 4],
            [8, 0, 3, 5]]
 
-difference = [[0, 0],
-              [0, 0]]
-dominant = [[0, 0],
-            [0, 0]]
+difference = [[0, 0, 0, 0],
+               [0, 0, 0, 0],
+               [0, 0, 0, 0],
+               [0, 0, 0, 0]]
+dominant = [[0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0]]
+
+##difference = [[0, 0],
+##              [0, 0]]
+##dominant = [[0, 0],
+##            [0, 0]]
 
 sillyPlayer1 = [[0, 0, 0, 0],
                 [0, 0, 0, 0],
@@ -45,27 +54,29 @@ sillyResult = [[0, 0, 0, 0],
 
 #Proposed method for producing entire solution checking for both dominant and silly solutions.
 def Main():
-    #differenceMethod()
-    #differenceComparision()
-    #print("Difference Array: ", difference)
-    #print("Dominant Strategies Bool Array: ", dominant)
-    
-    #greaterThanMethod()
+##    differenceMethod(player1, player2, difference)
+##    differenceComparision()
+##    print("Difference Array: ")
+##    printMatrix(difference)
+##    print("")
+##    print("Dominant Array: ")
+##    printMatrix(dominant)
+##    print("")
 
     greaterThanMethod(player1, sillyPlayer1)
     greaterThanMethod(player2, sillyPlayer2)
-    
-    print(sillyPlayer1)
-    print(sillyPlayer2)
 
-    print("")
+    print("Player 1: ")
     printMatrix(player1)
     print("")
+    print("Player 2: ")
     printMatrix(player2)
     print("")
     print("")
+    print("Silly Player 1 Results: ")
     printMatrix(sillyPlayer1)
     print("")
+    print("Silly Player 2 Results: ")
     printMatrix(sillyPlayer2)
     print("")
     
@@ -83,98 +94,36 @@ def Main():
 
 #Finds Dominant Intersection through Difference Calculations
 def differenceMethod(player1, player2, difference):
-    for x in range(2):
-        for y in range(2):
+    for x in range(len(player1 and player2)):
+        for y in range(len(player1 and player2)):
             difference[x][y] = abs((player1[x][y])-(player2[x][y]))
-            print("P1 ([{0}][{1}]): {2}".format(x,y,player1[x][y]))
-            print("P2 ([{0}][{1}]): {2}".format(x,y,player2[x][y]))
-            print("Difference: {0}".format(difference[x][y]))
+            #print("P1 ([{0}][{1}]): {2}".format(x,y,player1[x][y]))
+            #print("P2 ([{0}][{1}]): {2}".format(x,y,player2[x][y]))
+            #print("Difference: {0}".format(difference[x][y]))
             #print("Difference Array: ", difference) #If uncommented, show difference array progression through each iteration.
-            print("")
+            #print("")
 
 def differenceComparision():
-##    for x in range(2):
-##        for y in range(2):
-##            if difference[x][y] == difference[x][y]:
-##                dominant[x][y] = 1
-##                return
-##            else:
-##                dominant[x][y] = 0
-##                return
-
-##    for x in range(2):
-##        for y in range(2):
-##            if difference[x][y] == difference[x][y+1]:
-##                dominant[x][y] = 1
-##                print("([{0}][{1}]): True".format(x,y))
-##                return
-##            elif difference[x][y] == difference[x+1][y]:
-##                dominant[x][y] = 1
-##                print("([{0}][{1}]): True".format(x,y))
-##                return
-##            elif difference[x][y] == difference[x+1][y+1]:
-##                print("([{0}][{1}]): True".format(x,y))
-##                dominant[x][y] = 1
-##                return
-##            else:
-##                print("([{0}][{1}]): False".format(x,y))
-##                dominant[x][y] = 0
-##                return
-
-##    for x in range(2):
-##        for y in range(2):
-##            print("([{0}][{1}])".format(x,y))
-##            if not(y == 1):
-##                if difference[x][y] == difference[x][y+1]:
-##                    dominant[x][y] = 1
-##                    print("([{0}][{1}]): True".format(x,y))
-##                
-##            elif not(x == 1):
-##                if difference[x][y] == difference[x+1][y]:
-##                    dominant[x][y] = 1
-##                    print("([{0}][{1}]): True".format(x,y))
-##                
-##            elif not(x == 1 or y == 1):
-##                if difference[x][y] == difference[x+1][y+1]:
-##                    print("([{0}][{1}]): True".format(x,y))
-##                    dominant[x][y] = 1
-##                
-##            else:
-##                print("([{0}][{1}]): False".format(x,y))
-##                dominant[x][y] = 0
-
-##    for x in range(2):
-##        for y in range(2):
-##            print("([{0}][{1}])".format(x,y))
-##            if (y != 1) and (difference[x][y] == difference[x][y+1]):
-##                dominant[x][y] = 1
-##                print("([{0}][{1}]): True".format(x,y))
-##            elif (x != 1) and (difference[x][y] == difference[x+1][y]):
-##                dominant[x][y] = 1
-##                print("([{0}][{1}]): True".format(x,y))
-##            elif (y != 1 and x != 1) and (difference[x][y] == difference[x+1][y+1]):
-##                print("([{0}][{1}]): True".format(x,y))
-##                dominant[x][y] = 1
-##            else:
-##                print("([{0}][{1}]): False".format(x,y))
-##                dominant[x][y] = 0
-
-##    for x in range(2):
-##        for y in range(2):
-##            print("([{0}][{1}])".format(x,y))
-##            if (y != 1) and (difference[x][y] == difference[x][y+1]):
-##                dominant[x][y] = 1
-##                print("([{0}][{1}]): True".format(x,y))
-##            elif (x != 1) and (difference[x][y] == difference[x+1][y]):
-##                dominant[x][y] = 1
-##                print("([{0}][{1}]): True".format(x,y))
-##            elif (y != 1 and x != 1) and (difference[x][y] == difference[x+1][y+1]):
-##                print("([{0}][{1}]): True".format(x,y))
-##                dominant[x][y] = 1
-##            else:
-##                print("([{0}][{1}]): False".format(x,y))
-##                dominant[x][y] = 0
-    return
+    for x in range(len(player1 or player2)):
+        for y in range(len(player1 or player2)):
+            
+            print("([{0}][{1}])".format(x,y))
+            try: 
+                if (difference[x][y] == difference[x][y+1]):
+                    dominant[x][y] = 1
+                    print("([{0}][{1}]): True".format(x,y))
+                elif (difference[x][y] == difference[x+1][y]):
+                    dominant[x][y] = 1
+                    print("([{0}][{1}]): True".format(x,y))
+                elif (difference[x][y] == difference[x+1][y+1]):
+                    print("([{0}][{1}]): True".format(x,y))
+                    dominant[x][y] = 1
+                else:
+                    print("([{0}][{1}]): False".format(x,y))
+                    dominant[x][y] = 0
+            except:
+                pass
+    
 
 def greaterThanMethod(player, playerResult):
     for x in range(len(player)):
@@ -186,16 +135,15 @@ def greaterThanMethod(player, playerResult):
 
 def greaterThanComparision(playerResult1, playerResult2):
     #Find equilibrium by looking for matching 0 result values
-    for x in range(len(playerResult1 or playerResult2)):
-        for y in range(len(playerResult1 or playerResult2)):
+    for x in range(len(playerResult1 and playerResult2)):
+        for y in range(len(playerResult1 and playerResult2)):
             if playerResult1[x][y] == 0 and playerResult2[x][y] == 0:
                 print("Nach Equilibrium Found: ", x,y)
     return
 
 #Sub Procedures and Functions for UI elements
 def printMatrix(matrix):
-    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) 
-      for row in matrix]))
+    print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in matrix]))
 
 def userConfirm(question): #Require the user to confirm a qustion and return a boolean result
     reply = str(input(question+' (y/n): ')).lower().strip()
