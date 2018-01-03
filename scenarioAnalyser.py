21e6aa714c3b9bc5166bd483fe63fc0f6e195140
SammyHerring <sammy.herring@me.com>
2018-01-03 13:57:02 +0000

    v1.7
    Saddle Point Detection for 2x2 working. Working solution, excluding the
    ability for users to enter in their own matrices.

--- scenarioAnalyser.py
+++ scenarioAnalyser.py
@@ -45,42 +45,51 @@ stupidResult = [[0, 0, 0, 0],
                 [0, 0, 0, 0],
                 [0, 0, 0, 0]]
 
-equilibriumFound = False
-
 #Proposed method for producing entire solution checking for both dominant and stupid solutions.
 def Main():
-    print("\n--Program Start--\n")
     print("Game Theory Scenario Analyser")
     print("NB: Player Data Sets are entered by in a horizontal format\nrelative to their strategies.\n")
-    
-    printMatrix(player1, "Player 1 Data Set", True)
-    printMatrix(player2, "Player 2 Data Set", True)
 
-    print("Dominant Strategy Calculation")
-    print("For 2x2 Matrices Only")
-    differenceMethod(player12x2, player22x2, difference2x2)
-    differenceComparision()
-    printMatrix(difference2x2, "Difference Array", True)
-    printMatrix(dominantResult2x2, "Dominant Array", True)
+    selection = userConfirm("Select scenario to consider.\nEnter 1 for Example Posed or 2 for Saddle Point Example")
+    if selection == 1:
+            print("Running Scenario for Example Posed in Assignment using Stupid Strategies")
+            print("\n--Program Start--\n")
+            printMatrix(player1, "Player 1 Data Set", True)
+            printMatrix(player2, "Player 2 Data Set", True)
+            
+            print("NB: If you find that both players have a '0' Stupid Strategy Result this shows\na scenario where both players do not fail.\n")
 
-    print("Stupid Strategy Calculation")
-    print("If you find that both players have a '0' Stupid Strategy Result this shows\na scenario where both players do not fail.\n")
-    greaterThanMethod(player1, stupidPlayer1)
-    greaterThanMethod(player2, stupidPlayer2)
-    
-    printMatrix(stupidPlayer1, "Stupid Strategy Player 1 Results", True)
-    printMatrix(stupidPlayer2, "Stupid Strategy Player 2 Results", True)
-    
-    greaterThanComparision(player1, player2, stupidPlayer1, stupidPlayer2)
-    
-    #print("Stupid Strategies Bool Array: ", stupidResult)
+            greaterThanMethod(player1, stupidPlayer1)
+            greaterThanMethod(player2, stupidPlayer2)
+            
+            printMatrix(stupidPlayer1, "Stupid Strategy Player 1 Results", True)
+            printMatrix(stupidPlayer2, "Stupid Strategy Player 2 Results", True)
 
-    #NB: Task 3 Addition for potential saddle point calculation
+            greaterThanComparision(player1, player2, stupidPlayer1, stupidPlayer2)
+            end()
+    elif selection == 2:
+            print("Running Scenario for Example Posed in Assignment using Stupid Strategies")
+            print("\n--Program Start--\n")
+            printMatrix(player12x2, "Player 1 Data Set", True)
+            printMatrix(player22x2, "Player 2 Data Set", True)
+            
+            print("NB: Dominant Strategy Function For 2x2 Matrices Only\n")
+            
+            differenceMethod(player12x2, player22x2, difference2x2)
+            differenceComparision()
+            
+            printMatrix(difference2x2, "Difference Array", True)
+            printMatrix(dominantResult2x2, "Dominant Array", True)
+
+            dominantSolutionCheck(dominantResult2x2)
+            
+            end()
+    elif selection == 3:
+        pass
+    
     #probabilityCalculation()
     #probabilityComparision()
     
-    #completeComparision()
-    #result()
     end()
 
 #Finds Dominant Intersection through Difference Calculations
@@ -104,6 +113,16 @@ def differenceComparision():
             except:
                 pass
 
+def dominantSolutionCheck(dominantResult2x2):
+    noSolution = 0
+    for x in range(len(dominantResult2x2)):
+        for y in range(len(dominantResult2x2)):
+                if dominantResult2x2[x][y] == 0:
+                    noSolution += 1
+    if noSolution >= 4:
+        print("No dominant solution found. Saddle point detected.")
+                    
+
 def greaterThanMethod(player, playerResult):
     for x in range(len(player)):
         for y in range(len(player)):
@@ -118,13 +137,21 @@ def greaterThanComparision(player1, player2, playerResult1, playerResult2):
         for y in range(len(playerResult1 and playerResult2)):
             if playerResult1[x][y] == 0 and playerResult2[x][y] == 0:
                 print("Nach Equilibrium Found at Index: [{0}][{1}] with the values '{2}' and '{3}' respectively.".format(x, y, player1[x][y], player2[x][y]))
-                equilibriumFound = True
 
 #Sub Procedures and Functions for UI elements
 def printMatrix(matrix, title, newLine):
     print(title,":")
     print('\n'.join([''.join(['{:4}'.format(int(item)) for item in row]) for row in matrix]))
     if (newLine): print("")
+
+def userConfirm(question): #Require the user to select a question to solve
+    reply = str(input(question+': ')).lower().strip()
+    if reply[0] == '1':
+        return int(reply[0])
+    if reply[0] == '2':
+        return int(reply[0])
+    else:
+        return userConfirm("Please confirm using 1 or 2.")
 
 def end():
     print("\n--Program End--\n")
